3fc4a8b2058781c05d158ba4b534df2190117f66
Andrei Yangabishev <waffleboot@gmail.com>
2022-03-25 22:12:04 +0300

    использовать urfave

--- cmd/giiter/main.go
+++ cmd/giiter/main.go
@@ -10,6 +10,8 @@ import (
 	"os/exec"
 	"os/signal"
 	"syscall"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
@@ -22,7 +24,34 @@ func run() error {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	cmd := exec.CommandContext(ctx, "git", "log", `--pretty=format:%h`)
+	m := newManager()
+
+	app := &cli.App{
+		Name: "giiter",
+		Commands: []*cli.Command{
+			{
+				Name: "git",
+				Subcommands: []*cli.Command{
+					{
+						Name:   "list",
+						Action: m.gitList,
+					},
+				},
+			},
+		},
+	}
+
+	return app.RunContext(ctx, os.Args)
+}
+
+type manager struct{}
+
+func newManager() *manager {
+	return &manager{}
+}
+
+func (m *manager) gitList(ctx *cli.Context) error {
+	cmd := exec.CommandContext(ctx.Context, "git", "log", `--pretty=format:%h`)
 
 	stdout, err := cmd.Output()
 	if err != nil {
