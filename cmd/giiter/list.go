7a523e53fee720f33f530fe335ca9791e1c0214c
Andrei Yangabishev <waffleboot@gmail.com>
2022-04-23 23:52:02 +0300

    lint if-else to switch

--- cmd/giiter/list.go
+++ cmd/giiter/list.go
@@ -25,13 +25,15 @@ func listFeatureCommits(cmd *cobra.Command, args []string) error {
 
 	for i := range records {
 		record := records[i]
-		if record.IsNewCommit() {
+
+		switch {
+		case record.IsNewCommit():
 			fmt.Printf("%d) ++ %s %s\n", i+1, record.FeatureSHA, record.FeatureMsg.Subject)
-		} else if record.IsOldCommit() {
+		case record.IsOldCommit():
 			fmt.Printf("%d) -- %s [%s] %s\n", i+1, record.ReviewSHA, record.ReviewBranch, record.ReviewMsg.Subject)
-		} else if record.FeatureSHA != record.ReviewSHA {
+		case record.FeatureSHA != record.ReviewSHA:
 			fmt.Printf("%d) ** %s [%s] %s\n", i+1, record.FeatureSHA, record.ReviewBranch, record.FeatureMsg.Subject)
-		} else {
+		default:
 			fmt.Printf("%d) ok %s [%s] %s\n", i+1, record.FeatureSHA, record.ReviewBranch, record.FeatureMsg.Subject)
 		}
 	}
