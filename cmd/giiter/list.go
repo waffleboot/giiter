23cf4232e552f16e2e106bd8be5f50d2756d86d8
Andrei Yangabishev <waffleboot@gmail.com>
2022-04-23 12:19:44 +0300

    поменять формат list

--- cmd/giiter/list.go
+++ cmd/giiter/list.go
@@ -26,9 +26,9 @@ func listFeatureCommits(cmd *cobra.Command, args []string) error {
 	for i := range records {
 		record := records[i]
 		if record.IsNewCommit() {
-			fmt.Printf("%d) !! %s %s\n", i+1, record.FeatureSHA, record.FeatureMsg.Subject)
+			fmt.Printf("%d) ++ %s %s\n", i+1, record.FeatureSHA, record.FeatureMsg.Subject)
 		} else if record.IsOldCommit() {
-			fmt.Printf("%d) ?? %s [%s] %s\n", i+1, record.ReviewSHA, record.ReviewBranch, record.ReviewMsg.Subject)
+			fmt.Printf("%d) -- %s [%s] %s\n", i+1, record.ReviewSHA, record.ReviewBranch, record.ReviewMsg.Subject)
 		} else if record.FeatureSHA != record.ReviewSHA {
 			fmt.Printf("%d) ** %s [%s] %s\n", i+1, record.FeatureSHA, record.ReviewBranch, record.FeatureMsg.Subject)
 		} else {
